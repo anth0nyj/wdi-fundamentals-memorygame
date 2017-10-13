33fc3a435d958adc118047453a1ac8dd394f0d91
Anthony <ajtalvarez@gmail.com>
2017-10-13 06:51:52 -0400

    Created game logic.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
-var cardOne = "king";
-var cardTwo = "queen";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-console.log("User flipped " + cardThree);
-console.log("User flipped " + cardOne);
+var cardOne = cards[0];
+console.log("User flipped " + cardOne + ".");
+cardsInPlay.push(cardOne);
+//console.log("User's current hand: " + cardsInPlay.join(", ") + ".");
+
+var cardTwo = cards[2];
+console.log("User flipped " + cardTwo + ".");
+cardsInPlay.push(cardTwo);
+//console.log("User's current hand: " + cardsInPlay.join(", ") + ".");
+
+if (cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
