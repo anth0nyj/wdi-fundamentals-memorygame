852eff097279a6994561ffa5e498716b626c37c4
Anthony <ajtalvarez@gmail.com>
2017-10-16 04:53:21 -0400

    Created game logic, again. This time, I created the flipCard and checkForMatch functions. I also called them in the document for the sake of testing them.

--- js/main.js
+++ js/main.js
@@ -1,20 +1,27 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-console.log("User flipped " + cardOne + ".");
-cardsInPlay.push(cardOne);
-//console.log("User's current hand: " + cardsInPlay.join(", ") + ".");
-
-var cardTwo = cards[2];
-console.log("User flipped " + cardTwo + ".");
-cardsInPlay.push(cardTwo);
-//console.log("User's current hand: " + cardsInPlay.join(", ") + ".");
-
-if (cardsInPlay.length === 2){
+var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+		console.log("You found a match!");
 	} else {
-		alert("Sorry, try again.");
-	}
-}
+		console.log("Sorry, try again.");
+	};
+};
+
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId] + ".");
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2){
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+			checkForMatch();
+		} else {
+			alert("Sorry, try again.");
+			checkForMatch();
+		};
+	};
+};
+
+flipCard(0);
+flipCard(2);
